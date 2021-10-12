84b229b143c726d1626346a7c47cba591d86a562
marulita18 <crespo.mariel@gmail.com>
2021-10-12 10:15:48 +0200

    added dependencies

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const PORT = process.env.PORT || 4000;
+const corsMiddleWare = require("cors");
+const loggerMiddleWare = require("morgan");
+const PORT = 4000;
 const winesRouter = require("./routers/wines");
 
 const app = express();
 
+app.use(corsMiddleWare());
+app.use(loggerMiddleWare("dev"));
 app.use("/wines", winesRouter);
 
 app.listen(PORT, () => {
